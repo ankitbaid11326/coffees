d4837d4f24c6c5620d863e433bb2211e9057d94b
Ankit Baid <ankit.baid@olacabs.com>
2021-11-11 22:45:30 +0530

    first commit

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,43 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['Chocolate', 'Vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new HttpException(`coffee ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
